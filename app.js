d8fd59d82022d2d652e596f909065e625804d22e
Matheus <teteulopes2011@hotmail.com>
2023-01-18 20:08:40 -0300

    Renomeação do app.js e adição de html nas rotas.

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('servidor em desenvolvimento...');
+    res.sendFile(__dirname + "/html/index.html");
+})
+
+app.get("/sobre/:nome", function(req, res){
+    res.send('minha pagina sobre...'+req.params.nome);
 })
 
 app.listen(80, function(){
